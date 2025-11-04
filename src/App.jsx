5679d94e0182baec6c793247479ed6d1e1be07d7
yashodha-22uom <139736730+yashodha-22uom@users.noreply.github.com>
2025-11-04 17:22:48 +0530

    chatroom css updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import "./App.css";
 import ChatRoom from "./components/ChatRoom";
+import ChatRoomEnhanced from "./components/ChatRoomEnhanced";
 
 function App() {
-  return <ChatRoom />;
+  // Use ChatRoomEnhanced for SSL support with toggle
+  // Use ChatRoom for the original version (Member 1 & 2)
+  // Using ChatRoomEnhanced with SSL enabled (port 7443)
+  return <ChatRoomEnhanced />;
 }
 
 export default App;
